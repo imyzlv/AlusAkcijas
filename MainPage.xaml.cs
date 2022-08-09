5d12569133091b0430e70a1fd588e6197bf706e0
Imants Treidis <imants.treidis@gmail.com>
2022-08-09 12:32:21 +0300

    Get prices and discounts from Rimi

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -18,7 +18,7 @@ public partial class MainPage : ContentPage
 		else
 			CounterBtn.Text = $"Clicked {count} times";
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
+		//SemanticScreenReader.Announce(CounterBtn.Text);
 
 		AlusAkcijas.Services.BeerScraper.GetRimiBeers();
 	}
--- Services/BeerScraper.cs
+++ Services/BeerScraper.cs
@@ -8,42 +8,54 @@ namespace AlusAkcijas.Services
         public static async void GetRimiBeers()
         {
             HtmlWeb web = new HtmlAgilityPack.HtmlWeb();
-            //int pageCount = await RimiPageCount();
-            int totalBeers = 0;
-            //for (int i = 1; i <= pageCount; i++)
-            //{
-            //    string url = "https://www.rimi.lv/e-veikals/lv/produkti/alkoholiskie-dzerieni/alus/c/SH-1-12?page=" + i.ToString() + "&pageSize=100&query=%3Arelevance%3AallCategories%3ASH-1-12%3AassortmentStatus%3AinAssortment";
-            //    HtmlDocument doc = web.Load(url);
-            //    foreach (var item in doc.DocumentNode.SelectNodes("//p[@class='card__name']"))
-            //    {
-            //        Console.WriteLine(item.InnerText);
-            //        totalBeers++;
-            //    }
-            //}
-            //Console.WriteLine("Total beers: {0}", totalBeers);
-
-            string url = "https://www.rimi.lv/e-veikals/lv/produkti/alkoholiskie-dzerieni/alus/c/SH-1-12?page=1&pageSize=100&query=%3Arelevance%3AallCategories%3ASH-1-12%3AassortmentStatus%3AinAssortment";
-            HtmlDocument doc = web.Load(url);
-            foreach (var item in doc.DocumentNode.SelectNodes("//li[@class='product-grid__item']"))
+            int pageCount = await RimiPageCount();
+            for (int i = 1; i <= pageCount; i++)
             {
-                //string title = item.SelectSingleNode(".//li[@class='product-grid__item']").InnerText.Trim();
-
-                // load the beer names
-                string beerName = item.SelectSingleNode(".//p[@class='card__name']").InnerText.Trim('\r', '\n', '\t');
-                Console.Write(beerName);
-                Console.Write(" ");
-
-                // get pricing
-                double beerCost = 0;
-                var price = item.SelectSingleNode(".//div[@class='price-tag card__price']").InnerText.Trim('\n', '\t', '\r');
+                string url = "https://www.rimi.lv/e-veikals/lv/produkti/alkoholiskie-dzerieni/alus/c/SH-1-12?page=" + i.ToString() + "&pageSize=100&query=%3Arelevance%3AallCategories%3ASH-1-12%3AassortmentStatus%3AinAssortment";
+                HtmlDocument doc = web.Load(url);
+                foreach (var item in doc.DocumentNode.SelectNodes("//div[@class='card__details']"))
                 {
-                    // do some ugly cleanup of the pricing value
-                    price = price.TrimStart().TrimEnd();
-                    price = price.Replace("\n", "").Replace("\r", "").Replace("\t", "").Replace(" ", "");
-                    price = price.Remove(price.Length - 6);
+                    // load the beer names
+                    string beerName = null;
+                    if (item.SelectSingleNode(".//p[@class='card__name']") != null)
+                    {
+                        beerName = item.SelectSingleNode(".//p[@class='card__name']").InnerText.Trim('\r', '\n', '\t');
+                    }
+
+                    // load prices
+                    double beerCost = 0;
+
+                    if (item.SelectSingleNode(".//div[@class='price-tag card__price']") != null)
+                    {
+                        var price = item.SelectSingleNode(".//div[@class='price-tag card__price']").InnerText.Trim('\n', '\t', '\r');
+                        // do some ugly cleanup of the pricing value
+                        price = price.TrimStart().TrimEnd();
+                        price = price.Replace("\n", "").Replace("\r", "").Replace("\t", "").Replace(" ", "");
+                        price = price.Remove(price.Length - 6);
+                        beerCost = double.Parse(price) / 100;
+                    }
+                    // load dicounted prices
+                    double beerCostOld = 0;
+                    if (item.SelectSingleNode(".//div[@class='old-price-tag card__old-price']") != null)
+                    {
+                        var oldPrice = item.SelectSingleNode(".//div[@class='old-price-tag card__old-price']").InnerText.Trim('\n', '\t', '\r');
+                        oldPrice = oldPrice.TrimStart().TrimEnd();
+                        oldPrice = oldPrice.Replace("\n", "").Replace("\r", "").Replace("\t", "").Replace(" ", "");
+                        // remove the comma from old price
+                        oldPrice = oldPrice.Replace(",", "");
+                        //remove the EUR mark from the end
+                        oldPrice = oldPrice.Remove(oldPrice.Length - 1);
+                        beerCostOld = double.Parse(oldPrice) / 100;
+                    }
+
+                    if (beerCostOld > 0)
+                    {
+                        Console.Write(beerName);
+                        Console.Write(" Cena: ");
+                        Console.Write(beerCost);
+                        Console.WriteLine(" Vecā cena: {0}", beerCostOld);
+                    }
                 }
-                beerCost = double.Parse(price) / 100;
-                Console.WriteLine(beerCost);
             }
         }
 
