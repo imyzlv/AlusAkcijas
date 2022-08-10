fe32ed3ee4abd7d30089c316470fb3d862444fca
Imants Treidis <imants.treidis@gmail.com>
2022-08-10 16:32:45 +0300

    Beer with discounts and prices now being displayed on the screen

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,7 +2,6 @@
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
 
 	public MainPage()
 	{
@@ -11,14 +10,20 @@ public partial class MainPage : ContentPage
 
 	private async void OnCounterClicked(object sender, EventArgs e)
 	{
-		count++;
 		//SemanticScreenReader.Announce(CounterBtn.Text);
 
 		var beerList = await AlusAkcijas.Services.RimiBeerScraper.GetRimiBeers();
-		foreach(var beerItem in beerList)
+		foreach(var item in beerList)
         {
-			Console.WriteLine(beerItem.Name);
+			Console.WriteLine(item.Name);
         }
+	}
+
+	protected async override void OnAppearing()
+	{
+		base.OnAppearing();
+		var beers = await AlusAkcijas.Services.RimiBeerScraper.GetRimiBeers();
+		beerView.ItemsSource = beers;
 	}
 }
 
