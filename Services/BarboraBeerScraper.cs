72852c3c55c56c693877f694a628a15a41f5f538
Imants Treidis <imants.treidis@gmail.com>
2022-10-18 18:39:30 +0300

    Create BarboraBeerScraper.cs

---
+++ Services/BarboraBeerScraper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using HtmlAgilityPack;
+using AlusAkcijas.Models;
+
+namespace AlusAkcijas.Services
+{
+	public class BarboraBeerScraper
+	{
+		public static async Task<IEnumerable<Beer>> GetBarboraBeers()
+		{
+            HtmlWeb web = new HtmlAgilityPack.HtmlWeb();
+            List<Beer> beer = new List<Beer>();
+			return beer.ToList();
+        }
+	}
+}
+
